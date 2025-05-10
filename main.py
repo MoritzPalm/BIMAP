8055f1cbae52b904dd4949aca8728c1e70ef8a66
Moritz Palm <moritz.palm@web.de>
2025-05-10 10:51:05 +0200

    running ants on all frames from one experiment

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-import cv2
+import math
+
 import numpy as np
-from aicspylibczi import CziFile
 from pathlib import Path
 import matplotlib.pyplot as plt
 import ants
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import mean_squared_error
 
 pth = Path('./data/low_movement/Experiment-746czi')
 
 
 def main():
-    fixed_path = pth / 'frame_0000.tif'
-    moved_path = pth / 'frame_0400.tif'
-    fixed = ants.image_read(fixed_path.absolute().as_posix())
-    moved = ants.image_read(moved_path.absolute().as_posix())
-    fixed.plot(overlay=moved, title='Before Registration')
-    mytx = ants.registration(fixed=fixed, moving=moved, type_of_transform='SyN')
-    print(mytx)
-    warped_moving = mytx['warpedmovout']
-    fixed.plot(overlay=warped_moving, title='After Registration')
+    all_frames = list(pth.glob('frame_*.tif'))
+    all_frames = [x.as_posix() for x in all_frames]
+    all_frames.sort()
+
+    fixed = ants.image_read(all_frames[0])
+    motion_corrected = []
+    ssim_errors = []
+    mse_errors = []
+    for i in range(len(all_frames)):
+        moving = ants.image_read(all_frames[i])
+        areg = ants.registration(fixed, moving, 'SyN')
+        motion_corrected.append(areg['warpedmovout'])
+        ssim_errors.append(ssim(fixed.numpy(), moving.numpy(), data_range=moving.numpy().max() - moving.numpy().min()))
+        mse_errors.append(mean_squared_error(fixed.numpy(), moving.numpy()))
+    mean_ssim = sum(ssim_errors)/len(ssim_errors)
+    mean_mse = sum(mse_errors)/len(ssim_errors)
+    print(f"mean SSIM: {mean_ssim}")
+    print(f"mean MSE: {mean_mse}")
+    print(f"RMSE: {math.sqrt(mean_mse)}")
 
 
 if __name__ == "__main__":
