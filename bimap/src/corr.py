8b759cd4144d6b59fa1957c077d6c5065a53f679
Moritz Palm <moritz.palm@web.de>
2025-06-18 15:26:09 +0200

    fixed issues with file handling and added cotracker experiments

--- bimap/src/corr.py
+++ bimap/src/corr.py
@@ -12,8 +12,8 @@ from skimage.metrics import structural_similarity as ssim
 from tqdm import tqdm
 import cv2
 
-pth = Path("../../data/low_movement/Experiment-746czi")
-
+#pth = Path("../../data/low_movement/Experiment-746czi")
+pth =  Path("../../data/strong_movement/Experiment-591czi")
 
 def load_example_experiment() -> list[np.array]:
     """Load Experiment-746czi."""
@@ -22,7 +22,14 @@ def load_example_experiment() -> list[np.array]:
     if not frame_paths:
         error_msg = f"No files found matching {pattern=}"
         raise FileNotFoundError(error_msg)
-    return [np.array(Image.open(path.as_posix())).astype(np.float32) for path in frame_paths]
+    return [handle_raw_image(path) for path in frame_paths]
+
+
+def handle_raw_image(path: Path) -> np.ndarray:
+    image = Image.open(path.as_posix())
+    arr = np.array(image).byteswap().newbyteorder()
+    arr_f32 = arr.astype(np.float32) / 65535.0
+    return arr_f32
 
 
 def get_magnitude(img: np.array) -> np.array:
@@ -34,15 +41,14 @@ def get_magnitude(img: np.array) -> np.array:
 
 def find_highest_correlation(frame_stack: list[np.array], *, plot: bool =False) -> int:
     """Find frame with maximum correlation to previous frame."""
-    mean_corrs = []
+    corrs = []
     for i in range(len(frame_stack)-1):
-        corr_2d = np.corrcoef(frame_stack[i], frame_stack[i+1])
-        #np.corrcoef(...)
-        mean_corrs.append(np.mean(corr_2d))
-    max_idx = int(np.argmax(mean_corrs))
+        corr = np.corrcoef(frame_stack[i].flatten(), frame_stack[i+1].flatten())[0,1]
+        corrs.append(corr)
+    max_idx = int(np.argmax(corrs))
     if plot:
-        plt.plot(max_idx, mean_corrs[max_idx], "x")
-        plt.plot(mean_corrs)
+        plt.plot(max_idx, corrs[max_idx], "x")
+        plt.plot(corrs)
         plt.title("Correlation of each frame with the previous")
         plt.show()
     return max_idx
@@ -61,7 +67,7 @@ def ants_reg(frame_stack: list[np.array], template_idx: int) -> list[np.array]:
     return motion_corrected_images
 
 
-def evaluate(corrected_images: list[np.array], template: np.array) -> tuple[list, list]:
+def evaluate(corrected_images: list[np.array], template: np.array) -> tuple[list]:
     """Evaluate the image registration based on the SSIM of the gradient image."""
     ssim_list = [ssim(template, moving, data_range=template.max() - template.min()) for moving in corrected_images]
     gradient_ssim_list = []
@@ -80,6 +86,12 @@ def float32_to_uint8(image: np.array) -> np.array:
     return ((image - min_val) / (max_val - min_val) * 255.0).astype(np.uint8)
 
 
+def uint8_to_float32(image: np.array) -> np.array:
+    """Convert uint8 image type to float23 image type."""
+    image = image.astype(np.float32)
+    return image / 255.0
+
+
 def save_results(corrected_images: list[np.array], path: Path, method: str) -> None:
     """Save the results of the image registration."""
     save_path = path / (method + "_results")
@@ -92,4 +104,7 @@ def save_results(corrected_images: list[np.array], path: Path, method: str) -> N
 def denoise_stack(imgs: list[np.array]) -> np.array:
     imgs8 = [float32_to_uint8(img) for img in imgs]
     return [cv2.bilateralFilter(img8, d=10, sigmaColor=20, sigmaSpace=50) for img8 in imgs8]
+
+def normalize_stack(imgs: list[np.array]) -> np.array:
+    pass
 
